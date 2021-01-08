a8bc85f6e013da8d1a4bb38931138b2ef4f7c06c
SahanaSwamy574 <sahana.swamy@razorthink.com>
2021-01-08 13:41:20 +0530

    adding testing code

---
+++ debounce.test.js
@@ -0,0 +1,39 @@
+const debounce = (fn, delay) => {
+    let timeoutID;
+    return function (...args) {
+      if (timeoutID) {
+        clearTimeout(timeoutID);
+      }
+      timeoutID = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    };
+  };
+
+jest.useFakeTimers();
+
+describe('65593662', () => {
+  describe('debounce', () => {
+    it('should call function if timeoutID is undefined', () => {
+      const fn = jest.fn();
+
+      const debouncedFn = debounce(fn, 1000);
+      debouncedFn('param');
+      jest.advanceTimersByTime(1000);
+      expect(fn).toBeCalledWith('param');
+      expect(fn).toBeCalledTimes(1);
+    });
+
+    it('should not call function once if timeoutID exists', () => {
+      const fn = jest.fn();
+
+      const debouncedFn = debounce(fn, 1000);
+      debouncedFn('param');
+      debouncedFn('param');
+      debouncedFn('param');
+      jest.runAllTimers();
+      expect(fn).toBeCalledWith('param');
+      expect(fn).toBeCalledTimes(1);
+    });
+  });
+})
